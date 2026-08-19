3201b7cb2d538813f6fe2192b3044d662a545298
Minads-app <truonghyminh@gmail.com>
2026-08-20 03:33:04 +0700

    Auto sync: 2026-08-20 03:33:04

--- auto-push.bat
+++ auto-push.bat
@@ -26,8 +26,7 @@ echo 1. Dang kiem tra thay doi...
 git add .
 
 echo 2. Dang luu lich su...
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
-set datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
+for /f "delims=" %%a in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do set datetime=%%a
 git commit -m "Auto sync: %datetime%"
 
 echo 3. Dang day len GitHub...
